770853c1ac4e99f59ad17ffab54b4da7dfe3ce65
Sumit <sumitishere3@gmail.com>
2024-01-19 04:05:25 +0530

    Single-byte XOR cipher

---
+++ Set 1/challenge3.py
@@ -0,0 +1,11 @@
+"Single-byte XOR cipher"
+
+hex_string = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+
+import binascii
+def single_byte_xor(hex):
+    hexstring = binascii.unhexlify(hex_string)
+    result = (''.join(chr(num ^ key) for num in hexstring) for key in range(256))        
+    return max(result, key=lambda s: s.count(' '))
+
+print(single_byte_xor(hex_string))
